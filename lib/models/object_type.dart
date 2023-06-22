1e43180e5c95ac419c15bd82c85fa34f653a6446
GabPSS <45743069+GabPSS@users.noreply.github.com>
2023-06-21 21:16:41 -0300

    Added auxiliary functions to ObjectType and Report

--- lib/models/object_type.dart
+++ lib/models/object_type.dart
@@ -1,18 +1,31 @@
 import 'package:checkup_app/data/data_master.dart';
 import 'package:checkup_app/models/task.dart';
+import 'package:flutter/material.dart';
 
 class ObjectType {
   late int id;
   String name = "";
   List<int> _taskIds = List<int>.empty(growable: true);
 
-  ObjectType({required DataMaster dm}) {
+  DataMaster dm;
+
+  ObjectType({required this.dm}) {
     id = dm.objectTypeKey;
     dm.objectTypeKey++;
   }
 
-  List<Task> getTasks() {
-    throw UnimplementedError();
-    //TODO: Implement this later
+  List<Task> getTasks() => dm.tasks.where((element) => _taskIds.contains(element.id)).toList();
+
+  IconData getIcon() {
+    switch (name) {
+      case "PC":
+        return Icons.computer;
+      case "Phone":
+        return Icons.smartphone;
+      case "TV":
+        return Icons.tv;
+      default:
+        return Icons.token;
+    }
   }
 }
--- lib/models/report.dart
+++ lib/models/report.dart
@@ -1,4 +1,5 @@
 import 'package:checkup_app/models/location.dart';
+import 'package:checkup_app/models/report_answer.dart';
 
 import '../data/data_master.dart';
 
@@ -7,9 +8,29 @@ class Report {
   String name = "";
   List<Location> locations = List.empty(growable: true);
   int checkupObjectKey = 0;
+  
+  DataMaster dm;
 
-  Report({required DataMaster dm}) {
+  Report({required this.dm}) {
     id = dm.reportKey;
     dm.reportKey++;
+  }
+
+  int getObjectCount() {
+    int objectCount = 0;
+    for (var i = 0; i < locations.length; i++) {
+      objectCount += locations[i].objects.length;
+    }
+    return objectCount;
+  }
+
+  DateTime? getLastFilledDate() {
+    for (var i = 0; i < dm.reportAnswers.length; i++) {
+      var reportAnswer = dm.reportAnswers[i];
+      if (reportAnswer.id == id) {
+        return reportAnswer.answerDate;
+      }
+    }
+    return null;
   }
 }
