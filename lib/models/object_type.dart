c3e6c01f603227962a8381d63553d9f19990e020
GabPSS <45743069+GabPSS@users.noreply.github.com>
2023-07-13 17:55:34 -0300

    Added placeholder DM and objecttype editor pages

--- lib/models/object_type.dart
+++ lib/models/object_type.dart
@@ -5,16 +5,29 @@ import 'package:flutter/material.dart';
 class ObjectType {
   late int id;
   String name = "";
-  List<int> _taskIds = List<int>.empty(growable: true);
+  final List<int> _taskIds = List<int>.empty(growable: true);
 
   DataMaster dm;
 
   ObjectType({required this.dm}) {
     id = dm.objectTypeKey;
     dm.objectTypeKey++;
+    dm.objectTypes.add(this);
   }
 
   List<Task> getTasks() => dm.tasks.where((element) => _taskIds.contains(element.id)).toList();
+
+  void addTask(Task task) {
+    var id = task.id;
+    if (!_taskIds.contains(id)) _taskIds.add(id);
+  }
+
+  void removeTask(Task task) {
+    var id = task.id;
+    if (_taskIds.contains(id)) _taskIds.remove(id);
+  }
+
+  bool hasTask(Task task) => _taskIds.contains(task.id);
 
   IconData getIcon() {
     switch (name) {
--- lib/ui/main_object_types/main_object_types_page.dart
+++ lib/ui/main_object_types/main_object_types_page.dart
@@ -1,7 +1,11 @@
+import 'package:checkup_app/data/data_master.dart';
+import 'package:checkup_app/ui/main_object_types/object_type_editor_page.dart';
 import 'package:flutter/material.dart';
 
 class MainObjectTypesPage extends StatefulWidget {
-  const MainObjectTypesPage({super.key});
+  final DataMaster dm;
+
+  const MainObjectTypesPage({super.key, required this.dm});
 
   @override
   State<MainObjectTypesPage> createState() => _MainObjectTypesPageState();
@@ -10,6 +14,90 @@ class MainObjectTypesPage extends StatefulWidget {
 class _MainObjectTypesPageState extends State<MainObjectTypesPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    if (widget.dm.objectTypes.isNotEmpty) {
+      return ListView.builder(
+        itemBuilder: (context, index) {
+          var objectType = widget.dm.objectTypes[index];
+          return Card(
+            child: InkWell(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ObjectTypeEditorPage(
+                        dm: widget.dm,
+                        objectType: objectType,
+                        onUpdate: () => setState(() {}),
+                      ),
+                    ));
+              },
+              child: ListTile(
+                leading: CircleAvatar(
+                  child: Icon(objectType.getIcon()),
+                ),
+                title: Text(objectType.name),
+                trailing: PopupMenuButton(
+                  onSelected: (value) {
+                    switch (value) {
+                      case 0:
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ObjectTypeEditorPage(
+                                      dm: widget.dm,
+                                      objectType: objectType,
+                                      onUpdate: () => setState(() {}),
+                                    )));
+                        break;
+                      case 1:
+                        showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                            title: Text('Delete \'${objectType.name}\'?'),
+                            content: const Text("You won't be able to recover this object type once it's gone"),
+                            actions: [
+                              TextButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                    Navigator.pop(context);
+                                  },
+                                  child: const Text('Cancel')),
+                              TextButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                    setState(() => widget.dm.objectTypes.remove(objectType));
+                                  },
+                                  child: const Text('Delete'))
+                            ],
+                          ),
+                        );
+                        break;
+                      default:
+                    }
+                  },
+                  itemBuilder: (context) => [
+                    const PopupMenuItem(
+                        value: 0,
+                        child: ListTile(
+                          leading: Icon(Icons.edit),
+                          title: Text('Edit'),
+                        )),
+                    const PopupMenuItem(
+                        value: 1,
+                        child: ListTile(
+                          leading: Icon(Icons.delete),
+                          title: Text('Delete'),
+                        ))
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+        itemCount: widget.dm.objectTypes.length,
+      );
+    }
+
+    return Placeholder();
   }
 }
